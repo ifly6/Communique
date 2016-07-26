058b9b57482e9258ecd672b64dfc5aa2abf7c230
ifly6 <ifly6@me.com>
2016-07-25 20:56:12 -0400

    Externalised two final Strings

---
+++ src/com/git/ifly6/communique/ngui/CommuniqueMessages.java
@@ -0,0 +1,34 @@
+/* Copyright (c) 2016 ifly6
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+package com.git.ifly6.communique.ngui;
+
+public class CommuniqueMessages {
+
+	public static final String acknowledgement = "Developed by ifly6, contributing to the repository at "
+			+ "[github.com/iflycode/communique], also known as the nation Imperium Anglorum on NationStates."
+			+ "\n\nMy thanks to bug-testers Tinfect and Krypton Nova.";
+	public static final String licence = "Copyright (c) 2016 ifly6\nPermission is hereby granted, free of charge, to any person "
+			+ "obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software "
+			+ "without restriction, including without limitation the rights to use, copy, modify, merge, publish, "
+			+ "distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software "
+			+ "is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this "
+			+ "permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE "
+			+ "SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT "
+			+ "LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. "
+			+ "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, "
+			+ "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE "
+			+ "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+
+}
