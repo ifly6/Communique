7ee41c14ef2ba9dd04120c1b630e528a9c06e042
ifly6 <ifly6@me.com>
2016-09-22 17:27:53 -0400

    Bug fix
    When using the recruiter for long periods of time, it can fail to fetch
    a new recipient. That problem has been solved by telling it to try
    again indefinitely.

--- src/com/git/ifly6/communique/ngui/CommuniqueMessages.java
+++ src/com/git/ifly6/communique/ngui/CommuniqueMessages.java
@@ -19,6 +19,7 @@ public class CommuniqueMessages {
 	public static final String acknowledgement = "Developed by ifly6, contributing to the repository at "
 			+ "[github.com/iflycode/communique], also known as the nation Imperium Anglorum on NationStates."
 			+ "\n\nMy thanks to bug-testers Tinfect and Krypton Nova.";
+
 	public static final String licence = "Copyright (c) 2016 ifly6\nPermission is hereby granted, free of charge, to any person "
 			+ "obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software "
 			+ "without restriction, including without limitation the rights to use, copy, modify, merge, publish, "
@@ -30,5 +31,5 @@ public class CommuniqueMessages {
 			+ "IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, "
 			+ "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE "
 			+ "SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
-	
+
 }
