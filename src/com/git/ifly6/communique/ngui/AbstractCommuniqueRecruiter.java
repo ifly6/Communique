0d8350a51b2ad3d89ccdd56a59ce78a1ee984dc7
ifly6 <ifly6@me.com>
2016-10-08 20:20:02 -0400

    lots of bugs
    the filtering mechanism failed. it was fixed. a filter was added to the
    output for marconi’s sentList, which now gets rid of duplicates and
    deals with some systematic errors from the early days. The JInfoFetcher
    is now a singleton object. MarconiRecruiter now directly updates its
    own dynamic copy of the sentList, which it did not in the past (because
    I fucked up).

--- src/com/git/ifly6/communique/ngui/AbstractCommuniqueRecruiter.java
+++ src/com/git/ifly6/communique/ngui/AbstractCommuniqueRecruiter.java
@@ -17,6 +17,7 @@ package com.git.ifly6.communique.ngui;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -25,44 +26,43 @@ import com.git.ifly6.javatelegram.util.JInfoFetcher;
 import com.git.ifly6.javatelegram.util.JTelegramException;
 import com.git.ifly6.nsapi.NSNation;
 
-/** @author Kevin */
 public abstract class AbstractCommuniqueRecruiter {
 	
-	public static final JInfoFetcher fetcher = new JInfoFetcher();
-	
+	public static final JInfoFetcher fetcher = JInfoFetcher.getInstance();
+
 	protected String clientKey;
 	protected String secretKey;
 	protected String telegramId;
 	
 	protected List<String> recipients;
-	protected List<String> sentList;
+	protected LinkedHashSet<String> sentList;
 	protected Set<String> proscribedRegions;
-	
+
 	public void setWithCConfig(CConfig config) {
 		
 		setClientKey(config.keys.getClientKey());
 		setSecretKey(config.keys.getSecretKey());
 		setTelegramId(config.keys.getTelegramId());
-		
+
 		recipients = new ArrayList<>(Arrays.asList(config.recipients));
-		sentList = new ArrayList<>(Arrays.asList(config.sentList));
-		
+		sentList = new LinkedHashSet<>(Arrays.asList(config.sentList));
+
 	}
-	
+
 	public void setClientKey(String key) {
 		this.clientKey = key;
 	}
-	
+
 	public void setSecretKey(String key) {
 		this.secretKey = key;
 	}
-	
+
 	public void setTelegramId(String id) {
 		this.telegramId = id;
 	}
-	
+
 	public abstract void send();
-	
+
 	/** Returns a recipient based on the new recipients list from the NS API, filtered by whether it is proscribed. Note
 	 * that any issues or problems are dealt with my defaulting to the newest nation, ignoring the proscription filter.
 	 *
@@ -72,31 +72,29 @@ public abstract class AbstractCommuniqueRecruiter {
 		try {
 			
 			recipients = fetcher.getNew();
-
+			
 			for (String element : recipients) {
 				
 				boolean match = true;
-				if (sentList.contains(element)) {
-					match = false;
-				}
-				if (isProscribed(element, proscribedRegions)) {
-					match = false;
-				}
+				
+				// @formatter:off
+				if (sentList.contains(element)) { match = false; }
+				if (isProscribed(element)) { match = false; }
+
 				try {
-					if (new NSNation(element).populateData().isRecruitable()) {
-						match = false;
-					}
+					if (new NSNation(element).populateData().isRecruitable() == false) { match = false; }
 				} catch (IOException e) {
 					// do nothing and assume that the nation can be recruited if an exception is thrown
 				}
+				// @formatter:on
 				
 				// Return if match is still true
 				if (match) { return element; }
 			}
-
+			
 			// If the filtering failed, then simply just return the newest nation.
 			return recipients.get(0);
-
+			
 		} catch (JTelegramException e) {
 			e.printStackTrace();
 			System.err.println("Error. Retrying recipients list.");
@@ -105,14 +103,14 @@ public abstract class AbstractCommuniqueRecruiter {
 			return getRecipient();
 		}
 	}
-	
+
 	/** Determines whether a nation is in a region excluded by the JList <code>excludeList</code>. This method acts with
 	 * two assumptions: (1) it is not all right to telegram to anyone who resides in a prescribed region and (2) if they
 	 * moved out of the region since founding, it is certainly all right to do so.
 	 *
 	 * @param nationName
 	 * @return <code>boolean</code> on whether it is proscribed */
-	public boolean isProscribed(String element, Set<String> proscribedRegions) {
+	public boolean isProscribed(String element) {
 		
 		NSNation nation = new NSNation(element);
 		try {
@@ -121,13 +119,13 @@ public abstract class AbstractCommuniqueRecruiter {
 			// Failure to fetch information means false
 			return false;
 		}
-		
+
 		String nRegion = nation.getRegion().replaceAll(" ", "_");
 		for (String proscribedRegion : proscribedRegions) {
 			if (proscribedRegion.replaceAll(" ", "_").equalsIgnoreCase(nRegion)) { return true; }
 		}
-		
+
 		return false;
 	}
-	
+
 }
