9e4334ae847623053cffb041ce995c90f1c416ec
ifly6 <ifly6@me.com>
2016-12-16 15:28:07 -0500

    Worked on parser
    Started working on a revamp of the parsing backend so the program is in
    line with the same way NS declares its recipients using `nation:X` or
    `+region:Europe`. This change should allow for people to more easily
    understand the syntax.

--- src/com/git/ifly6/marconi/MarconiLauncher.java
+++ src/com/git/ifly6/marconi/MarconiLauncher.java
@@ -28,7 +28,7 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import com.git.ifly6.communique.CommuniqueParser;
+import com.git.ifly6.communique.data.CommuniqueParser;
 
 /**
  * @author Kevin
