7d7fb8b91ac031ae9aa971e46de700bb11934688
ifly6 <ifly6@me.com>
2016-12-16 15:28:07 -0500

    Worked on parser
    Started working on a revamp of the parsing backend so the program is in
    line with the same way NS declares its recipients using `nation:X` or
    `+region:Europe`. This change should allow for people to more easily
    understand the syntax.

--- src/com/git/ifly6/communique/data/CommuniqueParser.java
+++ src/com/git/ifly6/communique/data/CommuniqueParser.java
@@ -13,7 +13,7 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
-package com.git.ifly6.communique;
+package com.git.ifly6.communique.data;
 
 import java.util.ArrayList;
 import java.util.Arrays;
