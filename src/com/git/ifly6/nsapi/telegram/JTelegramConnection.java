b3d184e9142c09dfdfa0b6d2ea76f3879fad3154
ifly6 <ifly6@me.com>
2021-06-09 10:33:16 -0400

    fix queuing error on space in nation name

--- src/com/git/ifly6/nsapi/telegram/JTelegramConnection.java
+++ src/com/git/ifly6/nsapi/telegram/JTelegramConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 ifly6
+ * Copyright (c) 2021 ifly6
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this class file and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -73,6 +73,9 @@ public class JTelegramConnection {
                 keys.getSecretKey(),
                 keys.getTelegramId(),
                 recipient);
+        urlString = urlString.replace(" ", "%20");
+        LOGGER.info("Attempting URL: " + urlString);
+
         apiConnection = (HttpURLConnection) new URL(urlString).openConnection(); // always should be true
         apiConnection.setRequestProperty("User-Agent",
                 MessageFormat.format(
