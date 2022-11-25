97394f23001da476fbff4900517ba48c06626d5c
ifly6 <ifly6@me.com>
2022-11-25 01:00:31 -0500

    processing action introduced into Communique7Monitor
    also
    - refactoring and update docs in ApiUtils
    - optimised algorithm in CommuniqueProcessingAction#DELEGATE_PRIORITISE
    - updated documentation (peek -> preview) some time ago

--- src/com/git/ifly6/nsapi/ApiUtils.java
+++ src/com/git/ifly6/nsapi/ApiUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 ifly6
+ * Copyright (c) 2022 ifly6
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this class file and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -119,10 +119,12 @@ public class ApiUtils {
     }
 
     /**
-     * Shuffles array and returns it.
+     * Shuffles array and returns it. Always shuffles with the same {@link Random} source: {@link #RANDOM}.
+     * @param list to shuffle
+     * @returns shuffled list; can be ignored because {@link Collections#shuffle(List, Random)} acts in-place
      * @since version 3.0 (build 13)
      */
-    public static <T> List<T> shuffled(List<T> list) {
+    public static <T> List<T> shuffle(List<T> list) {
         Collections.shuffle(list, RANDOM);
         return list;
     }
