73c9ad86e1b4a09a760bb2726fb1a47139f06940
ifly6 <ifly6@me.com>
2024-04-02 22:39:00 -0400

    massively simplify Communique7Monitor

--- src/com/git/ifly6/communique/data/Communique7Monitor.java
+++ src/com/git/ifly6/communique/data/Communique7Monitor.java
@@ -18,129 +18,89 @@
 package com.git.ifly6.communique.data;
 
 import com.git.ifly6.communique.io.CommuniqueConfig;
-import com.git.ifly6.nsapi.ctelegram.monitors.CommMonitor;
+import com.git.ifly6.nsapi.ctelegram.monitors.CommUpdatableMonitor;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.OptionalLong;
-import java.util.Set;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 /**
  * Packages the recipients into a single monitor for reproducibility.
  * @since version 13
  */
-public class Communique7Monitor implements CommMonitor {
+public class Communique7Monitor extends CommUpdatableMonitor {
 
     private static final Logger LOGGER = Logger.getLogger(Communique7Monitor.class.getName());
     private static final int TRUNCATION_LIMIT = 6; // every 3 minutes
 
     private State state;
-    final private CommuniqueConfig theConfig;
 
-    private Set<String> alreadyPassed = new HashSet<>();
-    private List<String> lastParseResults;
-    private List<String> lastPassed;
+    private CommuniqueConfig theConfig;
+
+    private int updateCalls = 0;
+    private List<String> lastParse;
 
     public Communique7Monitor(CommuniqueConfig communiqueConfig) {
         this.theConfig = communiqueConfig;
         this.state = State.INIT;
     }
 
-    /**
-     * {@inheritDoc} All recipients are those defined by the appropriate input of {@link CommuniqueRecipient}s. It only
-     * passes a given recipient once. If {@link CommuniqueConfig#repeats} is true, it will truncate to providing the
-     * first {@value #TRUNCATION_LIMIT} recipients.
-     * <p>If {@link #preview()} is called before this method is first run – ie we are still initialising but {@link
-     * #lastParseResults} is not null – it will use the cached last parse results and filter them.</p>
-     * <p>See {@link #parseRecipients()} for application of
-     * {@link com.git.ifly6.communique.io.CommuniqueProcessingAction CommuniqueProcessingAction}s.</p>
-     */
-    public List<String> getRecipients() {
-        if (recipientsExhausted())
-            // we've already passed recipients and we do not repeat
-            // 2022-11-24. is the above comment true??
-            throw new ExhaustedException("Recipients exhausted!");
-
-        List<String> recipients = this.state == State.INIT && lastParseResults != null
-                ? preview() // if we are initialising AND cached the data already, use previewed data
-                : filtered(parseRecipients()); // otherwise generate for yourself
-
-        this.state = State.RUNNING;
-        lastPassed = theConfig.repeats
-                ? recipients.stream().limit(TRUNCATION_LIMIT).collect(Collectors.toList())
-                : recipients;
-        alreadyPassed.addAll(lastPassed);
-        return lastPassed;
-        // 2022-11-24. it seems this "lastPassed" var should really be called "toPass"
-        // id. it keeps track of the recipients that were LAST passed for dispatch
+    public Communique7Monitor setConfig(CommuniqueConfig theConfig) {
+        this.theConfig = theConfig;
+        return this;
     }
 
-    /**
-     * Peeks at the last parse results; if there are none, creates them.
-     * @return last parse results
-     * @throws UnsupportedOperationException if called outside of initialisation
-     * @see #getRecipients()
-     * @see #parseRecipients()
-     */
-    public List<String> peek() {
-        if (state != State.INIT) throw new UnsupportedOperationException("Cannot peek outside of initialisation");
-        if (lastParseResults == null) parseRecipients();
-        return lastParseResults;
+    @Override
+    protected List<String> getAction() {
+        return lastParse;
+    }
+
+    @Override
+    protected void updateAction() {
+        updateCalls++;
+        parseRecipients();
     }
 
     /**
      * Previews recipients the monitor expects to provide. If not loaded, loads them.
-     * @return {@link #peek()}, but after applying {@link #filtered(List)}
-     * @throws UnsupportedOperationException from {@link #peek()}
+     * @return first update list of recipients
+     * @throws UnsupportedOperationException if not in initialisation
      * @see #getRecipients()
      */
     public List<String> preview() {
-        return filtered(peek());
+        if (state != State.INIT) throw new UnsupportedOperationException("Cannot peek outside of initialisation");
+        if (lastParse == null) parseRecipients();
+        return lastParse;
     }
 
     /**
-     * Creates new provided list, with elements in that list which are contained by {@link #alreadyPassed} removed.
-     * @param list to filter
-     * @return filtered list
-     */
-    private List<String> filtered(List<String> list) {
-        return list.stream()
-                .filter(s -> !alreadyPassed.contains(s))
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Parses recipients based on the current configuration setting. Caches this information in
-     * {@link #lastParseResults}. Automatically filters out elements already passed.
-     * @return {@link #lastParseResults} after updating it from {@link Communique7Parser}
+     * Parses recipients based on the current configuration setting. Caches this information in {@link #lastParse}.
+     * @return {@link #lastParse} after updating it from {@link Communique7Parser}
      */
     private List<String> parseRecipients() {
-        this.lastParseResults = theConfig.processingAction.apply(
+        this.lastParse = theConfig.processingAction.apply(
                 new Communique7Parser().apply(theConfig.getcRecipients()).listRecipients());
 
-        LOGGER.finer(String.format("Monitor parsed %d recipients", lastParseResults.size()));
-        return this.lastParseResults;
+        LOGGER.finer(String.format("Monitor parsed %d recipients", lastParse.size()));
+        return this.lastParse;
     }
 
-    /** @return false if {@link CommuniqueConfig#repeats}; true otherwise. */
     @Override
     public boolean recipientsExhausted() {
-        // ifly6. 2022-11-24. shouldn't this never exhaust if we are repeating??
         if (this.state == State.INIT) return false;
-        return lastPassed.isEmpty();
+        if (!theConfig.repeats && updateCalls >= 1) return true;
+        return false;
     }
 
     /**
-     * {@inheritDoc} For Communique7Monitor, returns the size of {@link #lastParseResults}, ie all known possible
-     * recipients. This number can change over time if repeating.
+     * {@inheritDoc} For Communique7Monitor, returns the size of {@link #lastParse}, ie all known possible recipients.
+     * This number can change over time if repeating.
      * @return estimated number of remaining recipients; {@link OptionalLong#empty()} if repeating.
      */
     @Override
-    public OptionalLong recipientsCountIfKnown() {
+    public OptionalLong recipientsCount() {
         if (theConfig.repeats) return OptionalLong.empty();
-        return OptionalLong.of(lastParseResults.size());
+        return OptionalLong.of(lastParse.size());
     }
 
     /** Expresses the state – {@link #INIT} or {@link #RUNNING} – of the monitor. */
